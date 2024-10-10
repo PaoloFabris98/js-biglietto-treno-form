3083e07a654b6328a0f7a781c2a6512c5ed41c06
PaoloFabris98 <f98paolo@gmail.com>
2024-10-10 18:55:37 +0200

    feat:better

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const buttonSubmit = document.getElementById("submit");
-const errorData = document.getElementById("dataError")
+const errorData = document.getElementById("dataError");
 const succesData = document.getElementById("succes");
 let KM = document.getElementById("KM");
 let age = document.getElementById("Age");
@@ -7,7 +7,7 @@ let age = document.getElementById("Age");
 const minAge = 0;
 const maxAge = 120;
 const minKM = 0;
-const maxKM = 100
+const maxKM = 100;
 
 const underAge = 18;
 const overAge = 65;
@@ -17,39 +17,38 @@ const underAgeDiscount = 20;
 const overAgeDiscount = 40;
 let price = 0;
 
-console.log(errorData)
-
+console.log(errorData);
 
 buttonSubmit.addEventListener("click", function () {
+  let kmValue = Number(KM.value);
+  let ageValue = Number(age.value);
 
-    let kmValue = Number(KM.value);
-    let ageValue = Number(age.value);
-    
-    console.log("Kilometri:", kmValue);
-    console.log("Età:", ageValue);
-    
-    if (isNaN(kmValue) || (kmValue < minKM ) || (kmValue > maxKM)){
-        errorData.classList.remove("d-none");
-        errorData.innerHTML = errorData.innerHTML = `Il campo dei Chilometri accetta solo numeri da ${minKM} al ${maxKM}.`;;
-    } else if (isNaN(ageValue) || (ageValue < minAge) || (ageValue > maxAge)){
-        errorData.classList.remove("d-none");
-        errorData.innerHTML = `Il campo dell'età accetta solo numeri da ${minAge} al ${maxAge}.`;;
+  console.log("Kilometri:", kmValue);
+  console.log("Età:", ageValue);
+
+  if (isNaN(kmValue) || kmValue < minKM || kmValue > maxKM) {
+    errorData.classList.remove("d-none");
+    errorData.innerHTML =
+      errorData.innerHTML = `Il campo dei Chilometri accetta solo numeri da ${minKM} al ${maxKM}.`;
+  } else if (isNaN(ageValue) || ageValue < minAge || ageValue > maxAge) {
+    errorData.classList.remove("d-none");
+    errorData.innerHTML = `Il campo dell'età accetta solo numeri da ${minAge} al ${maxAge}.`;
+  } else {
+    if (ageValue < underAge) {
+      price = kmValue * kmPrice;
+      price = price - (price / 100) * underAgeDiscount;
+    } else if (ageValue > overAge) {
+      price = kmValue * kmPrice;
+      price = price - (price / 100) * overAgeDiscount;
     } else {
-        if (ageValue < underAge){
-            price = (kmValue*kmPrice);
-            price = price - ((price/100)*underAgeDiscount);
-        } else if (ageValue > overAge) {
-            price = (kmValue*kmPrice);
-            price = price - ((price/100)*overAgeDiscount);
-        } else {
-            price = (kmValue*kmPrice);
-        }
-        succesData.classList.remove("d-none");
-        succesData.innerHTML = succesData.innerHTML = `
+      price = kmValue * kmPrice;
+    }
+    succesData.classList.remove("d-none");
+    succesData.innerHTML = succesData.innerHTML = `
         <b>Chilometri: <b> ${kmValue} <br>
         <b>Età: <b> ${ageValue} <br>
         <b>Costo Chilometro: <b> ${kmPrice} <br>
         <b>Costo Biglietto: <b> ${price.toFixed(2)} €<br>
-        `;;
-    }
+        `;
+  }
 });
